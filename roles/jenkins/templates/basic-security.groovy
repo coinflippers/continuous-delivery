da80c065d0f247d51906599ce62ca817041407e6
Sigfast <ms224dx@student.lnu.se>
2017-12-13 14:57:26 +0100

    fix typo

--- roles/jenkins/templates/basic-security.groovy
+++ roles/jenkins/templates/basic-security.groovy
@@ -8,18 +8,18 @@ def users = hudsonRealm.getAllUsers()
 users_s = users.collect { it.toString() }
 
 // Create the admin user account if it doesn't already exist.
-if ("{{ jenkins_admin_username }}" in users_s) {
+if (admin in users_s) {
     println "Admin user already exists - updating password"
 
-    def user = hudson.model.User.get('{{ jenkins_admin_username }}');
-    def password = hudson.security.HudsonPrivateSecurityRealm.Details.fromPlainPassword('{{ jenkins_admin_password }}')
+    def user = hudson.model.User.get('admin');
+    def password = hudson.security.HudsonPrivateSecurityRealm.Details.fromPlainPassword('admin')
     user.addProperty(password)
     user.save()
 }
 else {
     println "--> creating local admin user"
 
-    hudsonRealm.createAccount('{{ jenkins_admin_username }}', '{{ jenkins_admin_password }}')
+    hudsonRealm.createAccount('admin', 'admin')
     instance.setSecurityRealm(hudsonRealm)
 
     def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
